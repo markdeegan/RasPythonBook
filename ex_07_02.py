9e292d7b4a07f8cdbcf68a5723cc71c8fb14c61f
Mark Deegan <mark.deegan@tudublin.ie>
2022-05-28 12:01:44 +0100

    Update ex_07_02.py

--- ex_07_02.py
+++ ex_07_02.py
@@ -4,3 +4,19 @@
 from tkinter import *
 
 class App:
+  def __init__(self, master):
+    frame = Frame(master)
+    frame.pack()
+    label1 = Label(frame, text='deg C')
+    label1.grid(row=0, column=0)
+    
+    button1 = Button(frame, text='Convert', command=self.convert)
+    button1.grid(row=1)
+    
+    def convert(self):
+      print('not yet implemented')
+      
+root = Tk()
+root.wm_title('Temp Converter')
+app = App(root)
+root = mainloop()
