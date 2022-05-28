3b7ba5fbdf45435c38343a88fb6e7b04eb722479
markdeegan <mark@deegan.net>
2022-05-28 17:10:05 +0100

    More code

--- ex_07_03.py
+++ ex_07_03.py
@@ -5,10 +5,10 @@ from tkinter import *
 
 class App:
   def __init__(self, master):
-    #define the celsius cariable
+    # define the celsius cariable
     self.c_var = DoubleVar()
 
-    #define the fahrenheit cariable
+    #define the fahrenheit variable
     self.result_var = DoubleVar()
 
     # create the Frame
@@ -19,14 +19,22 @@ class App:
     label1 = Label(frame, text='deg C')
     label1.grid(row=0, column=0)
 
-    #create label 2, fahrenheit
+    # create label 2, fahrenheit
     label2 = Label(frame, text='deg F')
-    label1.grid(row=1, column=1)
+    label1.grid(row=1, column=0)
+
+    # create the result label
+    label3 = Label(frame, textvariable=self.result.var)
+    label3.grid(row=1, column=1)
+
+    # create the convert button1
+    button1 = Button(frame, text='Convert', command=self.convert)
+    button1.grid(row=2, columnspan=2)
 
 
     #create the entry 1 field to accept temperature
     entry1 = Entry(frame, textvariable=self.c_var)
-    entry1.grid(row=1, column=0)
+    entry1.grid(row=1, column=1)
 
     # create button1 to convert
     button1 = Button(frame, text='Convert', command=self.convert)
