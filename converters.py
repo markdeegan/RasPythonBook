77659b63ab84b52dbb52fb6e28cd9f53b2c4700b
markdeegan <mark@deegan.net>
2022-05-28 18:04:19 +0100

    fix typo in description

--- converters.py
+++ converters.py
@@ -21,6 +21,6 @@ class ScaleAndOffsetConverter(ScaleConverter):
         return value * self.factor + self.offset
 
 c1 = ScaleConverter('inches', 'mm', 25)
-print(c1.descriotion())
+print(c1.description())
 print('converting 2 inches')
 print(str(c1.convert(2)) + c1.units_to)
