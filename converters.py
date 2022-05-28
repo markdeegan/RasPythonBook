8d8ab12f6df45598bff6d9cea8ce00f92b947f0f
markdeegan <mark@deegan.net>
2022-05-28 17:53:54 +0100

    Adding base-class ScaleCOnverter

--- converters.py
+++ converters.py
@@ -1,5 +1,13 @@
 # converters module
 
 class ScaleConverter:
+    def __init__(self, units_from, units_to, factor):
+        self.units_from = units_from
+        self.units_to   = units_to
+        self.factor     = factor
 
+    def description(self)
+        return 'Convert ' + self_units_from + 'to ' + self_units_to
 
+    def convert(self, value)
+        return value * self.factor
