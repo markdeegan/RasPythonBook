671e58fde09d047e12c34542969a55fa1d347b9e
markdeegan <mark@deegan.net>
2022-05-28 22:07:30 +0100

    Fix controls

--- ex_07_04.py
+++ ex_07_04.py
@@ -8,14 +8,8 @@ from converters import *
 
 class App:
   def __init__(self, master):
+    # create a ScaleAndOffsetConverter
     self.t_conv=ScaleAndOffsetConverter('C','F',1.8,32)
-
-
-    # define the celsius cariable
-    self.c_var = DoubleVar()
-
-    #define the fahrenheit variable
-    self.result_var = DoubleVar()
 
     # create the Frame
     frame = Frame(master)
@@ -25,9 +19,20 @@ class App:
     label1 = Label(frame, text='deg C')
     label1.grid(row=0, column=0)
 
-    # create label 2, fahrenheit
+    # define the celsius cariable
+    self.c_var = DoubleVar()
+
+    #create the entry 1 field to accept temperature
+    entry1 = Entry(frame, textvariable=self.c_var)
+    entry1.grid(row=1, column=1)
+
+    #create label 2, fahrenheit
     label2 = Label(frame, text='deg F')
-    label1.grid(row=1, column=0)
+    label2.grid(row=0, column=1)
+
+
+    #define the fahrenheit variable
+    self.result_var = DoubleVar()
 
     # create the result label
     label3 = Label(frame, textvariable=self.result_var)
@@ -36,15 +41,6 @@ class App:
     # create the convert button1
     button1 = Button(frame, text='Convert', command=self.convert)
     button1.grid(row=2, columnspan=2)
-
-
-    #create the entry 1 field to accept temperature
-    entry1 = Entry(frame, textvariable=self.c_var)
-    entry1.grid(row=1, column=1)
-
-    # create button1 to convert
-    button2= Button(frame, text='Convert', command=self.convert)
-    button2.grid(row=1)
 
   def convert(self):
       c = self.c_var.get()
